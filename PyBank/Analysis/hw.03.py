7213b14f08548b5f8ef5dcad4244e18bc3f6264b
Pete Turnham <peteturnham@gmail.com>
2021-01-15 09:17:47 -0500

    changed print statements

--- PyBank/Analysis/hw.03.py
+++ PyBank/Analysis/hw.03.py
@@ -8,38 +8,29 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     # save the csv data in csvreader
     next(csvreader)
-    #looping through each row
+    #storing data in row column
     rows = []
-    month_total = 0
+    #variable to store length of total months
     month_count = 0
-    month_average = 0
-    greatest_increase=0
-    greatest_decrease = 0
+    month_total = 0
+    def average(rows):
+        return sum(rows[1]) / len(rows)
+
     for row in csvreader:
         rows.append(row)
         #calulate total_months
         month_count = month_count +1
         #calculate monthly total
         month_total = (int(month_total) + int(row[1]))
-        #find greatest increase
-    #intialize variable for greatest profit increase
-    #if row is bigger than greatest profit
-    if int(row[1]) > greatest_increase:
-        #greatest profit is equal to row 1
-        greatest_increase = int(row[1])
-        #else, greatest profit is equal to 0
-    #if row is less than lowest profit
-    if int(row[1]) < greatest_decrease:
-        greatest_decrease = row[1]
-  
-    
-            
+
    
+
     print('Financial Analysis')
     print("---------------------------------")
     print('Total Months:', month_count)
     print('Total Month $: ', month_total)
-    print(greatest_decrease)
+    print(f'the Greatest Increase was', max(rows[1]))
+    print(f'the Greatest Decrese was', min(rows[1]))
 #print(rows)
 
 
