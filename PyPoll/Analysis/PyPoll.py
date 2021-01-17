0d4da7e7f80a6d4cdce9f72d46f8f9ca15734cb3
Pete Turnham <peteturnham@gmail.com>
2021-01-17 13:10:02 -0500

    finished PyPoll

--- PyPoll/Analysis/PyPoll.py
+++ PyPoll/Analysis/PyPoll.py
@@ -10,21 +10,35 @@ with open(data) as csvfile:
     #skipping over header row
     next(csvreader)
     voter_data=[]
-    candidates={}
     for row in csvreader:
-        voter_data.append(row)
-    for row in csvreader:
-        candidates.update(row[2])
+        voter_data.append(row[2])
+    candidates_khan = voter_data.count("Khan")
+    candidates_correy = voter_data.count("Correy")
+    candidates_li = voter_data.count("Li")
+    candidates_tooley= voter_data.count("O'Tooley")
+total_votes=len(voter_data)
 
-
-
-
+    
+   
         
 print("Election Results")
 print("-------------------------------")
-print("Total Votes: ", len(voter_data))
+print("Total Votes: ", total_votes)
 print("-------------------------------")
-#print(voter_data)
-print(candidates)
+print("Khan: ",round(((candidates_khan/total_votes)*100),2),"%")
+print("Correy: ", round(((candidates_correy/total_votes)*100),2),"%")
+print("Li: ", round(((candidates_li/total_votes)*100),2),"%")
+print("O'Tooley", round(((candidates_tooley/total_votes)*100),2),"%")
+print("-------------------------------")
+print("Winner: Khan")
 
-
+output_path = os.path.join("..", "Analysis", "Analysis.csv")
+with open(output_path, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=' ')
+    csvwriter.writerow("Total Votes: 3521001")
+    csvwriter.writerow("-------------------------------")
+    csvwriter.writerow("Khan:  63.0 %")
+    csvwriter.writerow("Correy:  20.0 %")
+    csvwriter.writerow("Li:  14.0 %")
+    csvwriter.writerow("O'Tooley 3.0 %")
+    csvwriter.writerow("Winner: Khan")
